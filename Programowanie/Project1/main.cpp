a6e51e2bc90e4a368a4023bc429190161086f317
AndrzejM <andrzejmmm1@gmail.com>
2022-11-27 22:53:37 +0100

    Programowanie Project1 - quickfix

--- Programowanie/Project1/main.cpp
+++ Programowanie/Project1/main.cpp
@@ -23,7 +23,7 @@ int GetColumnWidth()
 int GetColumnWidth() { return 80; }
 #endif
 
-void GetOptions();
+//void GetOptions();
 int GetPadding(int TotalWidth, char* s);
 void ShowGui();
 int GetInput(char** Text);
@@ -31,7 +31,8 @@ void Encrypt(char* Text, int InputLen);
 void Decrypt(char* Text, int InputLen);
 
 int main() {
-	GetOptions();
+	//GetOptions();
+	setlocale(LC_ALL, "");
 	int IsRunning = 1;
 	char Option = 0;
 	int InputLen;
@@ -52,8 +53,8 @@ int main() {
 			printf("\033[0;33m");
 			printf("\n%*s", TotalWidth / 2 + 7, "Szyfrowanie...\n");
 			InputLen = GetInput(&String);
-
-			Encrypt(String, InputLen);
+			if (InputLen > 0)
+				Encrypt(String, InputLen);
 
 			printf("\033[0m");
 			printf("\n%*s", TotalWidth / 2 + 15, "Naciœnij cokolwiek aby kontynuowaæ.");
@@ -63,8 +64,8 @@ int main() {
 			printf("\033[0;33m");
 			printf("\n%*s", TotalWidth / 2 + 7, "Deszyfrowanie...\n");
 			InputLen = GetInput(&String);
-
-			Decrypt(String, InputLen);
+			if (InputLen > 0)
+				Decrypt(String, InputLen);
 
 			printf("\033[0m");
 			printf("\n%*s", TotalWidth / 2 + 15, "Naciœnij cokolwiek aby kontynuowaæ.");
@@ -80,34 +81,34 @@ int main() {
 	return 0;
 }
 
-void GetOptions() {
-	setlocale(LC_ALL, "");
-	const char* FileName = "options.txt";
-	FILE* FileToRead;
-	char Znak[5];
-	FileToRead = fopen(FileName, "r");
-	if (FileToRead == NULL) {
-		printf("The options file is empty or does not exist.\n");
-
-		FILE* FileToWrite;
-		char Option[5];
-
-		FileToWrite = fopen(FileName, "w");
-		fprintf(FileToWrite, "");
-
-		fclose(FileToWrite);
-
-		FileToRead = fopen(FileName, "r");
-		fgets(Znak, 5, FileToRead);
-
-		fclose(FileToRead);
-	}
-	else {
-		fgets(Znak, 5, FileToRead);
-
-		fclose(FileToRead);
-	}
-}
+//void GetOptions() {
+//	setlocale(LC_ALL, "");
+//	const char* FileName = "options.txt";
+//	FILE* FileToRead;
+//	char Znak[5];
+//	FileToRead = fopen(FileName, "r");
+//	if (FileToRead == NULL) {
+//		printf("The options file is empty or does not exist.\n");
+//
+//		FILE* FileToWrite;
+//		char Option[5];
+//
+//		FileToWrite = fopen(FileName, "w");
+//		fprintf(FileToWrite, "");
+//
+//		fclose(FileToWrite);
+//
+//		FileToRead = fopen(FileName, "r");
+//		fgets(Znak, 5, FileToRead);
+//
+//		fclose(FileToRead);
+//	}
+//	else {
+//		fgets(Znak, 5, FileToRead);
+//
+//		fclose(FileToRead);
+//	}
+//}
 
 int GetPadding(int TotalWidth, char* s) {
 	int sWidth = strlen(s);
@@ -152,7 +153,7 @@ int GetInput(char** Text)
 	printf("\n%*s", TotalWidth / 2, "Dane wejœciowe: ");
 	printf("\033[0m");
 	fgets(Input, TabLen, stdin);
-	int InputLen = 0;
+	int InputLen = 0, error = 0;
 	for (int i = 0; i < TabLen; i++) {
 		if (Input[i] == 0 || Input[i] == 10)
 		{
@@ -161,6 +162,8 @@ int GetInput(char** Text)
 		//printf("%c, ", Input[i]);
 		InputLen++;
 	}
+	if (InputLen <= 0)
+		error = 2;
 	//printf("\nlen: %d\n", InputLen);
 	*Text = (char*)malloc(InputLen * sizeof(char));
 	for (int i = 0; i < InputLen * sizeof(char); i++) {
@@ -170,8 +173,22 @@ int GetInput(char** Text)
 		for (int i = 0; i < InputLen; i++) {
 			if (Input[i] >= 65 && Input[i] <= 90)
 				(*Text)[i] = Input[i] + 32;
+			else if (Input[i] != ' ' && Input[i] != '*' && Input[i] <= '/' || Input[i] >= ':' && Input[i] <= '`' || Input[i] >= '{') {
+				error = 1;
+				break;
+			}
 			else
 				(*Text)[i] = Input[i];
+		}
+	}
+	if (error) {
+		printf("\033[0;31m");
+		if (error == 1) {
+			printf("\n%*s", TotalWidth / 2 + 6, "B³êdne dane!");
+			InputLen = 0;
+		}
+		else if (error == 2) {
+			printf("\n%*s", TotalWidth / 2 + 7, "Puste wejœcie!");
 		}
 	}
 
@@ -181,116 +198,116 @@ int GetInput(char** Text)
 void Encrypt(char* Text, int InputLen) {
 	const int TotalWidth = GetColumnWidth();
 	//printf("%s", Text);
-	if (InputLen) {
-		int TabLen = InputLen * 4;
-		char* Encrypted = (char*)malloc(TabLen * sizeof(char));
-		int* Code = (int*)malloc(InputLen * sizeof(int));
-		int CurrIndex = 0, CurrElement = 1, CurrCode = 0, tmp[] = { 0,0 };
-		if (Encrypted) {
-			for (int i = 0; i < TabLen; i++) {
-				Encrypted[i] = NULL;
+	int TabLen = InputLen * 4;
+	char* Encrypted = (char*)malloc(TabLen * sizeof(char));
+	int* Code = (int*)malloc(InputLen * sizeof(int));
+	int CurrIndex = 0, CurrElement = 1, CurrCode = 0, tmp[] = { 0,0 };
+	if (Encrypted) {
+		for (int i = 0; i < TabLen; i++) {
+			Encrypted[i] = NULL;
+		}
+		for (int i = 0; i <= InputLen; i++) {
+			if (Text[i] == 'j' || Text[i] == 'q') {
+				Encrypted = "Nie ma mo¿liwoœci";
 			}
-			for (int i = 0; i <= InputLen; i++) {
-				if (Text[i] == 'j' || Text[i] == 'q')
-					Encrypted = "Nie ma mo¿liwoœci";
-			}
-			if (Encrypted[0] != 'N') {
-				while (CurrIndex < InputLen) {
-					//printf("E%d\n", CurrElement);
-					if (Elements[CurrElement][0] == Text[CurrIndex]) {
-						//printf("I%d\n", CurrIndex);
-						CurrIndex++;
-						if (ElementsLen[CurrElement] > 1) {
-							if (Elements[CurrElement][1] == Text[CurrIndex] && CurrIndex != InputLen) {
-								Code[CurrCode] = CurrElement;
-								CurrCode++;
-								CurrElement = 1;
-								CurrIndex++;
-							}
-							else {
-								CurrElement++;
-								CurrIndex--;
-							}
-						}
-						else {
+		}
+		if (Encrypted[0] != 'N') {
+			while (CurrIndex < InputLen) {
+				//printf("E%d\n", CurrElement);
+				if (Elements[CurrElement][0] == Text[CurrIndex]) {
+					//printf("I%d\n", CurrIndex);
+					CurrIndex++;
+					if (ElementsLen[CurrElement] > 1) {
+						if (Elements[CurrElement][1] == Text[CurrIndex] && CurrIndex != InputLen) {
 							Code[CurrCode] = CurrElement;
 							CurrCode++;
 							CurrElement = 1;
+							CurrIndex++;
+						}
+						else {
+							CurrElement++;
+							CurrIndex--;
 						}
 					}
-					else if (Text[CurrIndex] == ' ') {
-						CurrIndex++;
-						Code[CurrCode] = 0;
+					else {
+						Code[CurrCode] = CurrElement;
 						CurrCode++;
 						CurrElement = 1;
 					}
-					else {
-						CurrElement++;
+				}
+				else if (Text[CurrIndex] == ' ') {
+					CurrIndex++;
+					Code[CurrCode] = 0;
+					CurrCode++;
+					CurrElement = 1;
+				}
+				else {
+					CurrElement++;
+				}
+				if (CurrElement >= sizeof(ElementsLen) / 4) {
+					//printf("sizeof\n");
+					if (CurrIndex == 0 || Code[CurrCode - 1] >= sizeof(ElementsLen) / 4 - 1 || Code[CurrCode - 1] == 0) {
+						Encrypted = "Nie ma mo¿liwoœci";
+						break;
 					}
-					if (CurrElement >= sizeof(ElementsLen) / 4) {
-						//printf("sizeof\n");
-						if (CurrIndex == 0 || Code[CurrCode - 1] >= sizeof(ElementsLen) / 4 - 1 || Code[CurrCode - 1] == 0) {
-							Encrypted = "Nie ma mo¿liwoœci";
-							break;
-						}
-						else {
-							CurrIndex -= ElementsLen[Code[--CurrCode]];
-							CurrElement = Code[CurrCode] + 1;
-						}
+					else {
+						CurrIndex -= ElementsLen[Code[--CurrCode]];
+						CurrElement = Code[CurrCode] + 1;
 					}
 				}
 			}
-			if (Encrypted[0] != 'N') {
-				TabLen = CurrCode;
-				CurrIndex = 0;
-				for (int i = 0; i < TabLen; i++) {
-					//printf("C%d\n", Code[i]);
-					while (Code[i] >= 1) {
-						if (Code[i] >= 100) {
-							tmp[0] = Code[i] / 100;
-							tmp[1] = tmp[0] * 100;
-							Encrypted[CurrIndex] = tmp[0] + 48;
-							Code[i] -= tmp[1];
-							if (Code[i] < 1) {
-								Encrypted[++CurrIndex] = 48;
-							}
-							if (Code[i] < 10) {
-								Encrypted[++CurrIndex] = 48;
-							}
+		}
+		if (Encrypted[0] != 'N') {
+			TabLen = CurrCode;
+			CurrIndex = 0;
+			for (int i = 0; i < TabLen; i++) {
+				//printf("C%d\n", Code[i]);
+				while (Code[i] >= 1) {
+					if (Code[i] >= 100) {
+						tmp[0] = Code[i] / 100;
+						tmp[1] = tmp[0] * 100;
+						Encrypted[CurrIndex] = tmp[0] + 48;
+						Code[i] -= tmp[1];
+						if (Code[i] < 1) {
+							Encrypted[++CurrIndex] = 48;
 						}
-						else if (Code[i] >= 10) {
-							tmp[0] = Code[i] / 10;
-							tmp[1] = tmp[0] * 10;
-							Encrypted[CurrIndex] = tmp[0] + 48;
-							Code[i] -= tmp[1];
-							if (Code[i] < 1) {
-								Encrypted[++CurrIndex] = 48;
-							}
+						if (Code[i] < 10) {
+							Encrypted[++CurrIndex] = 48;
 						}
-						else {
-							tmp[0] = Code[i];
-							tmp[1] = tmp[0];
-							Encrypted[CurrIndex] = tmp[0] + 48;
-							Code[i] -= tmp[1];
-						}
-
-						CurrIndex++;
 					}
-					if (i != TabLen - 1)
-						Encrypted[CurrIndex] = '*';
+					else if (Code[i] >= 10) {
+						tmp[0] = Code[i] / 10;
+						tmp[1] = tmp[0] * 10;
+						Encrypted[CurrIndex] = tmp[0] + 48;
+						Code[i] -= tmp[1];
+						if (Code[i] < 1) {
+							Encrypted[++CurrIndex] = 48;
+						}
+					}
+					else {
+						tmp[0] = Code[i];
+						tmp[1] = tmp[0];
+						Encrypted[CurrIndex] = tmp[0] + 48;
+						Code[i] -= tmp[1];
+					}
+
 					CurrIndex++;
 				}
+				if (i != TabLen - 1)
+					Encrypted[CurrIndex] = '*';
+				CurrIndex++;
 			}
-
-			printf("\033[0;34m");
-			printf("%*s", TotalWidth / 2, "Dane wyjœciowe: ");
-			printf("\033[0m");
-			printf("%s\n", Encrypted);
 		}
-	}
-	else {
+
 		printf("\033[0;34m");
 		printf("%*s", TotalWidth / 2, "Dane wyjœciowe: ");
+		printf("\033[0m");
+		printf("%s\n", Encrypted);
+
+		if (Encrypted[0] != 'N') {
+			free(Encrypted);
+		}
+		free(Code);
 	}
 }
 
@@ -307,7 +324,7 @@ void Decrypt(char* Text, int InputLen) {
 			TabLen++;
 		}
 	}
-	TabLen *= 2;
+	TabLen *= 4;
 	//printf("%d", TabLen);
 	Decrypted = (char*)malloc(TabLen * sizeof(char));
 	if (Decrypted) {
@@ -351,4 +368,6 @@ void Decrypt(char* Text, int InputLen) {
 		printf("\033[0m");
 		printf("%s\n", Decrypted);
 	}
+	if (Decrypted[0] != 'N')
+		free(Decrypted);
 }
