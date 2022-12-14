f32a2afb6966b54862f28093edbf3b7741e9903a
AndrzejM <andrzejmmm1@gmail.com>
2022-12-14 23:19:00 +0100

    Programowanie Kol2

--- Programowanie/Sandbox/Source.cpp
+++ Programowanie/Sandbox/Source.cpp
@@ -4,12 +4,10 @@
 #pragma warning(disable : 4996)
 #define _CRT_SECURE_NO_WARNINGS
 #define _USE_MATH_DEFINES
-#include <math.h>
-#include <malloc.h>
 
 int main()
 {
 	setlocale(LC_ALL, "");
 
-	_getch();
+	return 0;
 }
