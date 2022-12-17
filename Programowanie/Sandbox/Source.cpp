742e91807358cfd84585f7723ba368692f1f6c86
WhiteStorm1 <andrzejmmm3@gmail.com>
2022-12-17 20:04:40 +0100

    Kol2 Update

--- Programowanie/Sandbox/Source.cpp
+++ Programowanie/Sandbox/Source.cpp
@@ -1,19 +1,26 @@
-#include<stdio.h>
-#include<conio.h>
-#include<locale.h>
-#include <malloc.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
 #pragma warning(disable : 4996)
 #define _CRT_SECURE_NO_WARNINGS
 #define _USE_MATH_DEFINES
 
-double** createTab(int m, int n);
-double* createTab_b(int m);
+struct Complex {
+    double real, img;
+};
 
 int main()
 {
-	setlocale(LC_ALL, "");
+    setlocale(LC_ALL, "");
+    Complex x,y;
 
-	return 0;
+    printf("Enter the real and imaginary parts of the first complex number: ");
+    scanf("%lf %lf", &x.real, &x.img);
+
+    printf("Enter the real and imaginary parts of the second complex number: ");
+    scanf("%lf %lf", &y.real, &y.img);
+
+    printf("(x*x + y*y) = %lf + %lfi\n", x.real * x.real + y.real * y.real - x.img * x.img - y.img * y.img, 2 * x.real * x.img + 2 * y.real * y.img);
+    _getch();
+    return 0;
 }
