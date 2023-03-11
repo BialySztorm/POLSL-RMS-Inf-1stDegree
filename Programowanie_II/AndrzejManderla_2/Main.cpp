ba098dec02f214b6270155b5b42f407f35433a7c
WhiteStorm <andrzejmmm1@gmail.com>
2023-03-11 19:51:46 +0100

    Programowanie II Zad 2

--- Programowanie_II/AndrzejManderla_2/Main.cpp
+++ Programowanie_II/AndrzejManderla_2/Main.cpp
@@ -6,22 +6,20 @@ int main() {
     double x, y, x0, y0, r;
 
     // wczytanie danych od u¿ytkownika z zabezpieczeniem przed wartoœciami mniejszymi b¹dŸ równymi 0
-    do {
-        std::cout << "Podaj wspolrzedne punktu (x y): ";
-        std::cin >> x >> y;
-    } while (x <= 0 || y <= 0);
+    std::cout << "Podaj wspolrzedne punktu (x y): ";
+    std::cin >> x >> y;
 
     do {
         std::cout << "Podaj wspolrzedne srodka kola (x0 y0) i promien r: ";
         std::cin >> x0 >> y0 >> r;
-    } while (x0 <= 0 || y0 <= 0 || r <= 0);
+    } while ( r <= 0);
 
     // utworzenie obiektów klas Punkt i Kolo
-    Punkt punkt(x, y);
-    Kolo kolo(x0, y0, r);
+    Point point(x, y);
+    Circle circle(x0, y0, r);
 
     // sprawdzenie, czy punkt nale¿y do ko³a i wyœwietlenie odpowiedniego komunikatu
-    if (kolo.punktNalezyDoKola(punkt)) {
+    if (circle.PointInCircle(point)) {
         std::cout << "Podany punkt nalezy do kola o srodku (" << x0 << "," << y0 << ") i promieniu " << r << std::endl;
     }
     else {
