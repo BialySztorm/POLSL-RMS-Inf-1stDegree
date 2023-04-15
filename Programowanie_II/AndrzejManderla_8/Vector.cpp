cbc2a60df1acfec38712d9a770045fd184187853
WhiteStorm <andrzejmmm1@gmail.com>
2023-04-15 14:14:41 +0200

    Programowanie II fix Zad 8

--- Programowanie_II/AndrzejManderla_8/Vector.cpp
+++ Programowanie_II/AndrzejManderla_8/Vector.cpp
@@ -1,29 +1,30 @@
 #include "Vector.h"
 #include <string.h>
 
-Vector::Vector(double x, double y, double z) : x(x), y(y), z(z) {}
+Vector1::Vector1(double x, double y, double z) : x(x), y(y), z(z) {}
 
-Vector Vector::operator-(const Vector& v) const
+Vector1 Vector1::operator-(const Vector1& v) const
 {
-    return Vector(x - v.x, y - v.y, z - v.z);
+    return Vector1(x - v.x, y - v.y, z - v.z);
 }
 
-Vector Vector::operator+(const Vector& v) const
+Vector1 Vector1::operator--()
 {
-    return Vector(x + v.x, y + v.y, z + v.z);
+    return Vector1(x-1, y-1, z-1);
 }
 
-Vector Vector::operator--()
-{
-    return Vector(x--, y--, z--);
-}
-
-Vector Vector::operator++()
-{
-    return Vector(x++, y++, z++);
-}
-
-std::string Vector::GetVector()
+std::string Vector1::GetVector()
 {
     return "(" + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z) + ")";
+}
+
+
+Vector1 operator+(const Vector1& v, const Vector1& v1)
+{
+    return Vector1(v1.x + v.x, v1.y + v.y, v1.z + v.z);
+}
+
+Vector1 operator++(const Vector1& v)
+{
+    return Vector1(v.x + 1, v.y + 1, v.z + 1);
 }
