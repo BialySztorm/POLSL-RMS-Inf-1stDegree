7df8ba8dd4ebe278dc5df37052463c7fbb75ad72
WhiteStorm <andrzejmmm1@gmail.com>
2023-11-04 20:29:06 +0100

    Programowanie III Project
    Added JFX functionality
    
    - Added import statements for necessary JavaFX classes
    - Created methods for encrypting and decrypting text
    - Updated FXML file to include text field and buttons for encryption, decryption, and exit functionality
    - Implemented event handlers for button clicks
    - Added CSS file for styling the application

--- Programowanie_III/Project2/src/main/java/com/whitestorm/project2/Controller.java
+++ Programowanie_III/Project2/src/main/java/com/whitestorm/project2/Controller.java
@@ -1,14 +1,33 @@
 package com.whitestorm.project2;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
+    Encryptor encryptor;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Działa");
+    private TextField textfield;
+
+    public Controller(){
+        encryptor = new Encryptor();
+    }
+    @FXML
+    protected void onEncryptButtonClick() {
+
+    }
+    @FXML
+    protected void onDecryptButtonClick()  {
+        textfield.setText(encryptor.Decrypt(textfield.getText()));
+    }
+    @FXML
+    protected void onExitButtonClick() {
+        Platform.exit();
     }
 }
--- Programowanie_III/Project2/src/main/java/com/whitestorm/project2/Main.java
+++ Programowanie_III/Project2/src/main/java/com/whitestorm/project2/Main.java
@@ -2,6 +2,7 @@ package com.whitestorm.project2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,12 +11,13 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),stage.getMaxWidth(),stage.getMaxHeight());
+        stage.setTitle("Szyfrator Mendelejewa");
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
