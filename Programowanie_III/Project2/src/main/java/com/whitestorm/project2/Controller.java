5e74d428dbbdb4bf7f744600ed5b396e2404119e
WhiteStorm <andrzejmmm1@gmail.com>
2023-11-04 15:20:13 +0100

    Programowanie 3 Project
    Add project files to .gitignore and configure IDE-specific settings
    
    This commit adds the following changes:
    - Added project files to .gitignore
    - Configured IntelliJ IDEA specific settings in .idea directory

---
+++ Programowanie_III/Project2/src/main/java/com/whitestorm/project2/Controller.java
@@ -0,0 +1,14 @@
+package com.whitestorm.project2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Działa");
+    }
+}
