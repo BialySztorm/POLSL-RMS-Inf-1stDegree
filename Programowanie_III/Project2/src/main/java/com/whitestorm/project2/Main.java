7d91b1278d86b8f07ce3d0187371124f4664f9ef
WhiteStorm <andrzejmmm1@gmail.com>
2023-11-06 00:45:59 +0100

    Programowanie III Project
    Add program icon and image resources
    
    This commit adds a program icon and an image resource to the project. The program now displays the icon in the window title bar and uses the image in a specific view.

--- Programowanie_III/Project2/src/main/java/com/whitestorm/project2/Main.java
+++ Programowanie_III/Project2/src/main/java/com/whitestorm/project2/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,6 +12,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Image icon = new Image(getClass().getResourceAsStream("Cipher.png"));
+        stage.getIcons().add(icon);
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),stage.getMaxWidth(),stage.getMaxHeight());
         stage.setTitle("Szyfrator Mendelejewa");
