e131b1c37f64259c88873f1030e341eecf48b03b
WhiteStorm <andrzejmmm1@gmail.com>
2023-11-19 09:46:21 +0100

    Programowanie III Lab 6
    Refactor sorting logic in Zad 3 and Dane class
    
    - In Zad 3, replaced lambda expression with Comparator.reverseOrder() to sort the array in reverse order.
    - In Dane class, modified compareTo() method to use Collator for comparing strings based on Polish locale and primary strength.
    
    Add sorting functionality to Main class
    
    - Imported Arrays and Comparator classes.
    - Sorted the spisTelefonow array using Arrays.sort() method with Comparator.reverseOrder().
    - Printed the sorted array.

--- Programowanie_III/Sandbox/src/Dane.java
+++ Programowanie_III/Sandbox/src/Dane.java
@@ -1,3 +1,6 @@
+import java.text.Collator;
+import java.util.Locale;
+
 class Dane implements Comparable<Dane> {
     private String nazwisko;
     private String numerTelefonu;
@@ -14,7 +17,10 @@ class Dane implements Comparable<Dane> {
 
     @Override
     public int compareTo(Dane o) {
-        return this.nazwisko.compareTo(o.nazwisko);
+        Locale locale = new Locale("pl","PL");
+        Collator col = Collator.getInstance(locale);
+        col.setStrength(Collator.PRIMARY);
+        return col.compare(this.nazwisko, o.nazwisko);
     }
 
     public String getNazwisko() {
